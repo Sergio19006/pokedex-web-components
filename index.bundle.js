796ba645faec2898cb4c9fd394b7f2eb75ae5186
Sergio del Pino <sergiodphn@gmail.com>
2024-08-07 12:07:32 +0100

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -9,12 +9,14 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-var Pokemon = /*#__PURE__*/_createClass(function Pokemon(name, image, url, id) {
+var Pokemon = /*#__PURE__*/_createClass(function Pokemon(name, image, url, id, types, weight) {
   _classCallCheck(this, Pokemon);
   this.name = name;
   this.url = url;
   this.image = image;
   this.id = id;
+  this.types = types;
+  this.weight = weight;
 });
 
 ;// CONCATENATED MODULE: ./src/grid/services/helpers/ImageSelector.js
@@ -70,6 +72,11 @@ var GridNormalizer = /*#__PURE__*/function () {
         var name = pokemon.name || ((_pokemon$pokemon = pokemon.pokemon) === null || _pokemon$pokemon === void 0 ? void 0 : _pokemon$pokemon.name) || ((_pokemon$pokemon_spec = pokemon.pokemon_species) === null || _pokemon$pokemon_spec === void 0 ? void 0 : _pokemon$pokemon_spec.name);
         return new Pokemon(name);
       });
+    }
+  }, {
+    key: "normalizePokemon",
+    value: function normalizePokemon(pokemon) {
+      return new Pokemon('', '', '', pokemon.id, pokemon.types, pokemon.weight);
     }
   }]);
   return GridNormalizer;
@@ -172,6 +179,40 @@ var GridDataProvider = /*#__PURE__*/function () {
       }
       return getPokemonsByCriterial;
     }()
+  }, {
+    key: "getPokemon",
+    value: function () {
+      var _getPokemon = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee3(id) {
+        var response, data;
+        return _regeneratorRuntime().wrap(function _callee3$(_context3) {
+          while (1) switch (_context3.prev = _context3.next) {
+            case 0:
+              _context3.prev = 0;
+              _context3.next = 3;
+              return fetch("https://pokeapi.co/api/v2/pokemon/".concat(id));
+            case 3:
+              response = _context3.sent;
+              _context3.next = 6;
+              return response.json();
+            case 6:
+              data = _context3.sent;
+              return _context3.abrupt("return", this._normalizer.normalizePokemon(data));
+            case 10:
+              _context3.prev = 10;
+              _context3.t0 = _context3["catch"](0);
+              console.error('There was an error!', _context3.t0);
+              throw new Error('There was an error fetching the pokemons');
+            case 14:
+            case "end":
+              return _context3.stop();
+          }
+        }, _callee3, this, [[0, 10]]);
+      }));
+      function getPokemon(_x2) {
+        return _getPokemon.apply(this, arguments);
+      }
+      return getPokemon;
+    }()
   }]);
   return GridDataProvider;
 }();
@@ -248,12 +289,37 @@ var GridManager = /*#__PURE__*/function () {
       }
       return getPokemonsByCriterial;
     }()
+  }, {
+    key: "getPokemon",
+    value: function () {
+      var _getPokemon = GridManager_asyncToGenerator( /*#__PURE__*/GridManager_regeneratorRuntime().mark(function _callee3(id) {
+        return GridManager_regeneratorRuntime().wrap(function _callee3$(_context3) {
+          while (1) switch (_context3.prev = _context3.next) {
+            case 0:
+              _context3.next = 2;
+              return this._provider.getPokemon(id);
+            case 2:
+              return _context3.abrupt("return", _context3.sent);
+            case 3:
+            case "end":
+              return _context3.stop();
+          }
+        }, _callee3, this);
+      }));
+      function getPokemon(_x2) {
+        return _getPokemon.apply(this, arguments);
+      }
+      return getPokemon;
+    }()
   }]);
   return GridManager;
 }();
 
 ;// CONCATENATED MODULE: ./src/grid/components/card/index.js
 function card_typeof(o) { "@babel/helpers - typeof"; return card_typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, card_typeof(o); }
+function card_regeneratorRuntime() { "use strict"; /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/facebook/regenerator/blob/main/LICENSE */ card_regeneratorRuntime = function _regeneratorRuntime() { return e; }; var t, e = {}, r = Object.prototype, n = r.hasOwnProperty, o = Object.defineProperty || function (t, e, r) { t[e] = r.value; }, i = "function" == typeof Symbol ? Symbol : {}, a = i.iterator || "@@iterator", c = i.asyncIterator || "@@asyncIterator", u = i.toStringTag || "@@toStringTag"; function define(t, e, r) { return Object.defineProperty(t, e, { value: r, enumerable: !0, configurable: !0, writable: !0 }), t[e]; } try { define({}, ""); } catch (t) { define = function define(t, e, r) { return t[e] = r; }; } function wrap(t, e, r, n) { var i = e && e.prototype instanceof Generator ? e : Generator, a = Object.create(i.prototype), c = new Context(n || []); return o(a, "_invoke", { value: makeInvokeMethod(t, r, c) }), a; } function tryCatch(t, e, r) { try { return { type: "normal", arg: t.call(e, r) }; } catch (t) { return { type: "throw", arg: t }; } } e.wrap = wrap; var h = "suspendedStart", l = "suspendedYield", f = "executing", s = "completed", y = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} var p = {}; define(p, a, function () { return this; }); var d = Object.getPrototypeOf, v = d && d(d(values([]))); v && v !== r && n.call(v, a) && (p = v); var g = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(p); function defineIteratorMethods(t) { ["next", "throw", "return"].forEach(function (e) { define(t, e, function (t) { return this._invoke(e, t); }); }); } function AsyncIterator(t, e) { function invoke(r, o, i, a) { var c = tryCatch(t[r], t, o); if ("throw" !== c.type) { var u = c.arg, h = u.value; return h && "object" == card_typeof(h) && n.call(h, "__await") ? e.resolve(h.__await).then(function (t) { invoke("next", t, i, a); }, function (t) { invoke("throw", t, i, a); }) : e.resolve(h).then(function (t) { u.value = t, i(u); }, function (t) { return invoke("throw", t, i, a); }); } a(c.arg); } var r; o(this, "_invoke", { value: function value(t, n) { function callInvokeWithMethodAndArg() { return new e(function (e, r) { invoke(t, n, e, r); }); } return r = r ? r.then(callInvokeWithMethodAndArg, callInvokeWithMethodAndArg) : callInvokeWithMethodAndArg(); } }); } function makeInvokeMethod(e, r, n) { var o = h; return function (i, a) { if (o === f) throw new Error("Generator is already running"); if (o === s) { if ("throw" === i) throw a; return { value: t, done: !0 }; } for (n.method = i, n.arg = a;;) { var c = n.delegate; if (c) { var u = maybeInvokeDelegate(c, n); if (u) { if (u === y) continue; return u; } } if ("next" === n.method) n.sent = n._sent = n.arg;else if ("throw" === n.method) { if (o === h) throw o = s, n.arg; n.dispatchException(n.arg); } else "return" === n.method && n.abrupt("return", n.arg); o = f; var p = tryCatch(e, r, n); if ("normal" === p.type) { if (o = n.done ? s : l, p.arg === y) continue; return { value: p.arg, done: n.done }; } "throw" === p.type && (o = s, n.method = "throw", n.arg = p.arg); } }; } function maybeInvokeDelegate(e, r) { var n = r.method, o = e.iterator[n]; if (o === t) return r.delegate = null, "throw" === n && e.iterator["return"] && (r.method = "return", r.arg = t, maybeInvokeDelegate(e, r), "throw" === r.method) || "return" !== n && (r.method = "throw", r.arg = new TypeError("The iterator does not provide a '" + n + "' method")), y; var i = tryCatch(o, e.iterator, r.arg); if ("throw" === i.type) return r.method = "throw", r.arg = i.arg, r.delegate = null, y; var a = i.arg; return a ? a.done ? (r[e.resultName] = a.value, r.next = e.nextLoc, "return" !== r.method && (r.method = "next", r.arg = t), r.delegate = null, y) : a : (r.method = "throw", r.arg = new TypeError("iterator result is not an object"), r.delegate = null, y); } function pushTryEntry(t) { var e = { tryLoc: t[0] }; 1 in t && (e.catchLoc = t[1]), 2 in t && (e.finallyLoc = t[2], e.afterLoc = t[3]), this.tryEntries.push(e); } function resetTryEntry(t) { var e = t.completion || {}; e.type = "normal", delete e.arg, t.completion = e; } function Context(t) { this.tryEntries = [{ tryLoc: "root" }], t.forEach(pushTryEntry, this), this.reset(!0); } function values(e) { if (e || "" === e) { var r = e[a]; if (r) return r.call(e); if ("function" == typeof e.next) return e; if (!isNaN(e.length)) { var o = -1, i = function next() { for (; ++o < e.length;) if (n.call(e, o)) return next.value = e[o], next.done = !1, next; return next.value = t, next.done = !0, next; }; return i.next = i; } } throw new TypeError(card_typeof(e) + " is not iterable"); } return GeneratorFunction.prototype = GeneratorFunctionPrototype, o(g, "constructor", { value: GeneratorFunctionPrototype, configurable: !0 }), o(GeneratorFunctionPrototype, "constructor", { value: GeneratorFunction, configurable: !0 }), GeneratorFunction.displayName = define(GeneratorFunctionPrototype, u, "GeneratorFunction"), e.isGeneratorFunction = function (t) { var e = "function" == typeof t && t.constructor; return !!e && (e === GeneratorFunction || "GeneratorFunction" === (e.displayName || e.name)); }, e.mark = function (t) { return Object.setPrototypeOf ? Object.setPrototypeOf(t, GeneratorFunctionPrototype) : (t.__proto__ = GeneratorFunctionPrototype, define(t, u, "GeneratorFunction")), t.prototype = Object.create(g), t; }, e.awrap = function (t) { return { __await: t }; }, defineIteratorMethods(AsyncIterator.prototype), define(AsyncIterator.prototype, c, function () { return this; }), e.AsyncIterator = AsyncIterator, e.async = function (t, r, n, o, i) { void 0 === i && (i = Promise); var a = new AsyncIterator(wrap(t, r, n, o), i); return e.isGeneratorFunction(r) ? a : a.next().then(function (t) { return t.done ? t.value : a.next(); }); }, defineIteratorMethods(g), define(g, u, "Generator"), define(g, a, function () { return this; }), define(g, "toString", function () { return "[object Generator]"; }), e.keys = function (t) { var e = Object(t), r = []; for (var n in e) r.push(n); return r.reverse(), function next() { for (; r.length;) { var t = r.pop(); if (t in e) return next.value = t, next.done = !1, next; } return next.done = !0, next; }; }, e.values = values, Context.prototype = { constructor: Context, reset: function reset(e) { if (this.prev = 0, this.next = 0, this.sent = this._sent = t, this.done = !1, this.delegate = null, this.method = "next", this.arg = t, this.tryEntries.forEach(resetTryEntry), !e) for (var r in this) "t" === r.charAt(0) && n.call(this, r) && !isNaN(+r.slice(1)) && (this[r] = t); }, stop: function stop() { this.done = !0; var t = this.tryEntries[0].completion; if ("throw" === t.type) throw t.arg; return this.rval; }, dispatchException: function dispatchException(e) { if (this.done) throw e; var r = this; function handle(n, o) { return a.type = "throw", a.arg = e, r.next = n, o && (r.method = "next", r.arg = t), !!o; } for (var o = this.tryEntries.length - 1; o >= 0; --o) { var i = this.tryEntries[o], a = i.completion; if ("root" === i.tryLoc) return handle("end"); if (i.tryLoc <= this.prev) { var c = n.call(i, "catchLoc"), u = n.call(i, "finallyLoc"); if (c && u) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } else if (c) { if (this.prev < i.catchLoc) return handle(i.catchLoc, !0); } else { if (!u) throw new Error("try statement without catch or finally"); if (this.prev < i.finallyLoc) return handle(i.finallyLoc); } } } }, abrupt: function abrupt(t, e) { for (var r = this.tryEntries.length - 1; r >= 0; --r) { var o = this.tryEntries[r]; if (o.tryLoc <= this.prev && n.call(o, "finallyLoc") && this.prev < o.finallyLoc) { var i = o; break; } } i && ("break" === t || "continue" === t) && i.tryLoc <= e && e <= i.finallyLoc && (i = null); var a = i ? i.completion : {}; return a.type = t, a.arg = e, i ? (this.method = "next", this.next = i.finallyLoc, y) : this.complete(a); }, complete: function complete(t, e) { if ("throw" === t.type) throw t.arg; return "break" === t.type || "continue" === t.type ? this.next = t.arg : "return" === t.type ? (this.rval = this.arg = t.arg, this.method = "return", this.next = "end") : "normal" === t.type && e && (this.next = e), y; }, finish: function finish(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.finallyLoc === t) return this.complete(r.completion, r.afterLoc), resetTryEntry(r), y; } }, "catch": function _catch(t) { for (var e = this.tryEntries.length - 1; e >= 0; --e) { var r = this.tryEntries[e]; if (r.tryLoc === t) { var n = r.completion; if ("throw" === n.type) { var o = n.arg; resetTryEntry(r); } return o; } } throw new Error("illegal catch attempt"); }, delegateYield: function delegateYield(e, r, n) { return this.delegate = { iterator: values(e), resultName: r, nextLoc: n }, "next" === this.method && (this.arg = t), y; } }, e; }
+function card_asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+function card_asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { card_asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { card_asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 function card_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function card_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, card_toPropertyKey(descriptor.key), descriptor); } }
 function card_createClass(Constructor, protoProps, staticProps) { if (protoProps) card_defineProperties(Constructor.prototype, protoProps); if (staticProps) card_defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
@@ -271,6 +337,7 @@ function _defineProperty(obj, key, value) { key = card_toPropertyKey(key); if (k
 function card_toPropertyKey(arg) { var key = card_toPrimitive(arg, "string"); return card_typeof(key) === "symbol" ? key : String(key); }
 function card_toPrimitive(input, hint) { if (card_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (card_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
+
 var Card = /*#__PURE__*/function (_HTMLElement) {
   _inherits(Card, _HTMLElement);
   var _super = _createSuper(Card);
@@ -279,6 +346,8 @@ var Card = /*#__PURE__*/function (_HTMLElement) {
     card_classCallCheck(this, Card);
     _this = _super.call(this);
     _this.pokemon = _this.getAttribute('pokemon');
+    _this._manager = new GridManager();
+    _this.loadInfo = false;
     return _this;
   }
   card_createClass(Card, [{
@@ -300,6 +369,37 @@ var Card = /*#__PURE__*/function (_HTMLElement) {
       (_this$querySelector2 = this.querySelector('.card__image')) === null || _this$querySelector2 === void 0 || _this$querySelector2.classList.remove('hidden');
     }
   }, {
+    key: "loadPokeInfo",
+    value: function () {
+      var _loadPokeInfo = card_asyncToGenerator( /*#__PURE__*/card_regeneratorRuntime().mark(function _callee() {
+        var info;
+        return card_regeneratorRuntime().wrap(function _callee$(_context) {
+          while (1) switch (_context.prev = _context.next) {
+            case 0:
+              if (!this.loadInfo) {
+                _context.next = 2;
+                break;
+              }
+              return _context.abrupt("return");
+            case 2:
+              _context.next = 4;
+              return this._manager.getPokemon(this.pokemon.id);
+            case 4:
+              info = _context.sent;
+              this.renderPokemonInfo(info);
+              this.loadInfo = true;
+            case 7:
+            case "end":
+              return _context.stop();
+          }
+        }, _callee, this);
+      }));
+      function loadPokeInfo() {
+        return _loadPokeInfo.apply(this, arguments);
+      }
+      return loadPokeInfo;
+    }()
+  }, {
     key: "template",
     value: function template() {
       this.innerHTML = "\n      <div class=\"card\">\n        <div class=\"card__loading card__text\">Loading...</div>\n          <img class=\"card__image hidden\" loading=\"lazy\" src=\"".concat(this.pokemon.image, "\" alt=\"").concat(this.pokemon.name, "\"/>\n          <span class=\"card__text\">\n            ").concat(this.pokemon.name, "\n          </span>\n      </div>");
@@ -309,6 +409,18 @@ var Card = /*#__PURE__*/function (_HTMLElement) {
     value: function render() {
       this.template();
       this.querySelector('.card__image').addEventListener('load', this.removeSkeleton.bind(this));
+      this.querySelector('.card__image').addEventListener('mouseover', this.loadPokeInfo.bind(this));
+    }
+  }, {
+    key: "renderPokemonInfo",
+    value: function renderPokemonInfo(pokemonInfo) {
+      var info = document.createElement('div');
+      info.classList.add('card__info');
+      info.innerHTML = "\n      <span> id: ".concat(pokemonInfo.id, " </span>\n        <div class=\"card__info__types\">\n          ").concat(pokemonInfo.types.map(function (_ref) {
+        var type = _ref.type;
+        return "<span class=\"".concat(type.name, "\"> \n              ").concat(type.name, "\n            </span>");
+      }), " \n        </div>\n      <span> peso:").concat(pokemonInfo.weight, " </span>\n    ");
+      this.querySelector('.card').appendChild(info);
     }
   }]);
   return Card;
